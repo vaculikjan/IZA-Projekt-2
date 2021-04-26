21863d18fa5097dc77c97ec0787b812fb5f89d0c
Jan Vaculik <vacuulikjan@gmail.com>
2021-04-26 22:31:35 +0200

    Refactor

--- SwiftUI_test/Extensions.swift
+++ SwiftUI_test/Extensions.swift
@@ -5,4 +5,8 @@
 //  Created by Jan Vaculik on 26.04.2021.
 //
 
-import Foundation
+extension String {
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9-\\s]", options: .regularExpression) == nil
+    }
+}
