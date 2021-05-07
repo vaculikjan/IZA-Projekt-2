714242fd51f2eae6974e1194fbf9878878d49206
Jan Vaculik <vacuulikjan@gmail.com>
2021-05-07 23:33:44 +0200

    Added match history

---
+++ SwiftUI_test/HistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  HistoryView.swift
+//  SwiftUI_test
+//
+//  Created by Jan Vaculik on 05.05.2021.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
---
+++ SwiftUI_test/MatchHistory.swift
@@ -0,0 +1,8 @@
+//
+//  MatchHistory.swift
+//  SwiftUI_test
+//
+//  Created by Jan Vaculik on 05.05.2021.
+//
+
+import Foundation
